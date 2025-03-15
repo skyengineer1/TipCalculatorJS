29bc8b4a5b12471fe71554ef8f2d735ded3c4aa6
Sky <skyengineer001@gmail.com>
2025-03-15 22:53:03 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 15/100 : bill * 20/100;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
